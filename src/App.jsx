6ac75668336e85861b28a39f85d321c916ba1913
hasib1511 <hasib7143@gmail.com>
2023-04-01 04:20:24 +0600

    display all blog dynamically in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Blogs from './componens/blogs/Blogs'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Blogs from "./componens/blogs/Blogs";
+import Tostify from "./Tostify/Tostify";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <Blogs></Blogs>
-        <h1 className='text-orange-700'>hasib</h1>
-      
     </div>
-  )
+  );
 }
-export default App
+export default App;
--- src/componens/blogs/Blogs.jsx
+++ src/componens/blogs/Blogs.jsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import SingleBlog from '../singleBlog/SingleBlog';
+import React from "react";
+import { useEffect, useState } from "react";
+
+import BookMark from "../BookMark/BookMark";
+import Header from "../Header/Header";
+import SingleBlog from "../single_blog/SingleBlog";
+import Toastify from "../Toastify/Toastify.jsx";
+//
 
 const Blogs = () => {
-    const [blogs, setBlogs] = useState([])
-    useEffect(() => {
-      fetch('data.json')
-    .then(res=>res.json())
-    .then(data => setBlogs(data))
-      
-    }, [])
+  const [isExist, setIsExist] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+  const [markedBlogs, setMarkedBlogs] = useState([]);
 
-    return (
-        <div>
-            <h1>hadsfld</h1>
-            {
-                blogs.map(blog => <SingleBlog></SingleBlog>)
-            }
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
+  return (
+    <div>
+      <div className="md:flex ">
+        <div className="md:w-8/12">
+          {blogs.map((blog) => (
+            <SingleBlog
+              addMin={addMin}
+              handleBookmark={handleBookmark}
+              key={blog.id}
+              blog={blog}
+            ></SingleBlog>
+          ))}
         </div>
-    );
+        <div className="md:w-4/12 inline ">
+          <BookMark min={min} markedBlogs={markedBlogs}></BookMark>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
